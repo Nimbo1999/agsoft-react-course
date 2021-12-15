7804165d350dbde866d693198b4da6a6ab0391c5
Matheus Lopes <matlopes1999@gmail.com>
2021-12-15 14:25:42 -0300

    Adiciona o primeiro componente e seus estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import ListItem from "./components/ListItem/ListItem";
+import ShoppingBag from "./assets/icons/ShoppingBag";
+
 function App() {
-  return <h1>Hello word</h1>;
+  return (
+    <div className="main-application">
+      <header>
+        <div>
+          <ShoppingBag />
+          <h2>Nimbo Store</h2>
+        </div>
+
+        <nav>
+          <ul>
+            <ListItem label="Products" />
+            <ListItem label="Shopping Cart" />
+            <ListItem label="Sign in" />
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default App;
