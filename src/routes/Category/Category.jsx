1cdc6b750ef1058d2c249b2d31efba326055fd41
Matheus Lopes <matlopes1999@gmail.com>
2021-12-23 14:07:25 -0300

    Inicia o componente de produto

--- src/routes/Category/Category.jsx
+++ src/routes/Category/Category.jsx
@@ -3,6 +3,14 @@ import { useParams } from 'react-router-dom';
 
 import { getCategoryProducts } from '../../firebase';
 
+const product = {
+    id: '',
+    image: 'https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+    name: '3 seater sofa belgrade',
+    price: 1320.23,
+    smallDescription: 'Host with warmth and good taste'
+};
+
 const Category = () => {
     const [products, setProducts] = useState([]);
     const { token } = useParams();
@@ -13,11 +21,19 @@ const Category = () => {
             setProducts(response);
         };
 
-        getData();
+        // getData();
     }, []);
 
     return (
-        <div className="category-container">{products.map(product => JSON.stringify(product))}</div>
+        <div className="category-container">
+            <main className="product-item">
+                <div className="image-wrapper">
+                    <img src={product.image} onLoad={() => console.log('Loaded!!!!')} />
+                </div>
+
+                <section>content</section>
+            </main>
+        </div>
     );
 };
 
